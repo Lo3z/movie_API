1658769a8e3595b41f4b04d85ae485a3087338b5
Lo3z <carlosheins1@gmail.com>
2025-07-24 18:26:54 -0400

    corrected mongoose connect uri

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const Users = Models.User;
 const app = express();
 
 // mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB');
-mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true}); 
+mongoose.connect('process.env.CONNECTION_URI', {useNewUrlParser: true, useUnifiedTopology: true}); 
 
 app.use(cors());
 
